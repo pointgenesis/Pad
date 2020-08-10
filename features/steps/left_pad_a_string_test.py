321652ed4c6e7564b8ff53f2b501d1b050543dfc
Travis Steinmetz <travissteinmetz@Traviss-MacBook-Pro.local>
2020-08-09 23:20:18 -0400

    development|travis|refactoring including swapping print for logger, static methods, etc.

--- features/steps/left_pad_a_string_test.py
+++ features/steps/left_pad_a_string_test.py
@@ -1,6 +1,6 @@
 
 from behave import given, when, then
-from pad import left
+from pad import Pad
 
 @given(u'a source string of value 100')
 def initialize_source_string(context):
@@ -16,13 +16,13 @@ def initialize_overall_length(context):
 
 @when(u'I invoke the left method with valid parameters')
 def create_fixed_length_left_padded_string(context):
-    assert(context.source_string == None) is False
-    assert(context.padding_character == None) is False
-    assert(context.output_length == None or context.output_length == 0) is False
+    assert(context.source_string is None) is False
+    assert(context.padding_character is None) is False
+    assert(context.output_length is None or context.output_length == 0) is False
     
-    context.left_padded_output = left(context.source_string, context.padding_character, context.output_length)
+    context.left_padded_output = Pad.left(context.source_string, context.padding_character, context.output_length)
     print("left_padded_output: " + context.left_padded_output)
-    assert(context.left_padded_output == None) is False
+    assert(context.left_padded_output is None) is False
 
 @then(u'I expect to receive a string with value 0000000100 in the response')
 def verify_string(context):
--- pad.py
+++ pad.py
@@ -8,88 +8,110 @@ References:
 [1] https://wiki.python.org/moin/HandlingExceptions
 [2] https://docs.python-guide.org/writing/structure/
 [3] https://stackoverflow.com/questions/3289601/null-object-in-python
+[4] https://realpython.com/python-logging/#basic-configurations
 '''
 
-import sys
+import logging
 from builtins import int, isinstance
 
-def is_none_or_empty(string):
-    """
+log = logging.getLogger(f'__main__{__name__}')
 
-    """
 
-def is_valid_input(source_string, padding_character, output_length):
-    '''
-    Method used to provide basic validation on input parameters. It checks for None aka null values,
-    ensures the proper type is presented, determines if the input is an empty string, and does basic
-    data validation such as length checking. It is used by both .left() and .right() padding operations.
-    '''
-    valid_input = True
-    
-    if not source_string or not isinstance(source_string, str) or source_string == '':
-        print('source_string cannot be None or empty')
+class Pad:
+    def __init__(self):
+        log.info(f'initializing {self.__class__.__name__}')
+
+    @staticmethod
+    def is_none_or_empty(target):
+        """
+        Provides basic string validation, verifying that the input is a string data type, is a valid object, and is not
+        emtpy.
+        :return: True if the input is not None and is a non-empty string; otherwise, returns False
+        """
         valid_input = False
-    
-    if not padding_character or not isinstance(padding_character, str) or padding_character == '':
-        print('padding_character cannot be None or empty')
-        valid_input = False
-        
-    if not isinstance(output_length, int) or output_length <  len(source_string):
-        print(
-            'output_length cannot be less than the length of source_string ' +
-            'output_length: ' + str(output_length) + ' ' +
-            'source_string: ' + str(source_string)
-             )
-        valid_input = False
-        
-    return valid_input
+        try:
+            if target:
+                if isinstance(target, str):
+                    if target == '':
+                        log.warning('target: >%s< Issue: is empty.', target)
+                    else:
+                        valid_input = True
+                else:
+                    log.warning('target: %s Issue: is not a str', target)
+            else:
+                log.warning('target: %s Issue: is None', target)
+        except Exception as e:
+            log.warning('Unexpected error encountered. Input is assumed invalid. Error: %s', e)
 
-def left(source_string, padding_character, output_length):
-    '''
-    source_string - the string to pad
-    padding_character - the character to pad the given source_string with
-    output_length - the overall length of the padded string
-    '''
-    left_padded_string = ''
-    
-    try:
-        if is_valid_input(source_string, padding_character, output_length):
-            print(
-                'input parameters -> source_string: ' + source_string + ' | ' + 
-                'padding_character: ' + padding_character + ' | ' +
-                'output_length: ' + str(output_length) 
-                )
-        
-            left_padded_string = padding_character[0]*(output_length - len(source_string)) + source_string
-        else:
-            print('Invalid input received, aborting padding operation.')
-            
-        return left_padded_string 
-            
-    except:
-        print('Unexpected error: ', sys.exc_info()[0])
+        return valid_input
 
-def right(source_string, padding_character, output_length):
-    '''
-    source_string - the string to pad
-    padding_character - the character to pad the given source_string with
-    output_length - the overall length of the padded string
-    '''
-    right_padded_string = ''
-    
-    try:
-        if is_valid_input(source_string, padding_character, output_length):
-            print(
-                'input parameters -> source_string: ' + source_string + ' | ' + 
-                'padding_character: ' + padding_character + ' | ' +
-                'output_length: ' + str(output_length)
-                )
-        
-            right_padded_string = source_string + padding_character[0]*(output_length - len(source_string))
-        else:
-            print('Invalid input received, aborting padding operation.')
-            
-        return right_padded_string
-    
-    except:
-        print('Unexpected error: ', sys.exc_info()[0])
+    @staticmethod
+    def is_valid_input(source_string, padding_character, output_length):
+        '''
+        Method used to provide basic validation on input parameters. It checks for None aka null values,
+        ensures the proper type is presented, determines if the input is an empty string, and does basic
+        data validation such as length checking. It is used by both .left() and .right() padding operations.
+        '''
+        valid_input = True
+
+        if not Pad.is_none_or_empty(source_string):
+            log.warning('source_string cannot be None or empty')
+            valid_input = False
+
+        if not Pad.is_none_or_empty(padding_character):
+            log.warning('padding_character cannot be None or empty')
+            valid_input = False
+
+        if not isinstance(output_length, int) or output_length < len(source_string):
+            log.warning('output_length cannot be less than the length of source_string - output_length: %s source_string: %s', output_length, source_string)
+            valid_input = False
+
+        return valid_input
+
+    @staticmethod
+    def left(source_string, padding_character, output_length):
+        '''
+        source_string - the string to pad
+        padding_character - the character to pad the given source_string with
+        output_length - the overall length of the padded string
+        '''
+        left_padded_string = ''
+
+        try:
+            if Pad.is_valid_input(source_string, padding_character, output_length):
+                log.debug('source_string: %s padding_character: %s output_length: %s', source_string,
+                                                                                       padding_character,
+                                                                                       output_length)
+                left_padded_string = padding_character[0]*(output_length - len(source_string)) + source_string
+                log.debug('left_padded_string: %s', left_padded_string)
+            else:
+                log.warning('Invalid input received, aborting padding operation.')
+
+            return left_padded_string
+
+        except Exception as e:
+            log.warning('WARNING: %s', e)
+
+    @staticmethod
+    def right(source_string, padding_character, output_length):
+        '''
+        source_string - the string to pad
+        padding_character - the character to pad the given source_string with
+        output_length - the overall length of the padded string
+        '''
+        right_padded_string = ''
+
+        try:
+            if Pad.is_valid_input(source_string, padding_character, output_length):
+                log.debug('source_string: %s padding_character: %s output_length: %s', source_string,
+                                                                                       padding_character,
+                                                                                       output_length)
+                right_padded_string = source_string + padding_character[0]*(output_length - len(source_string))
+                log.debug('right_padded_string: %s', right_padded_string)
+            else:
+                log.warning('Invalid input received, aborting padding operation.')
+
+            return right_padded_string
+
+        except Exception as e:
+            log.warning('WARNING: %s', e)
